bb9e0d659f17ccc3c6a0c8295b71775496fac523
LuckyNitmz <163147772+LuckyNitmz@users.noreply.github.com>
2025-10-04 18:39:11 +0530

    location pointer symbol

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -1,6 +1,15 @@
 import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
+
+// Fix for default marker icons in React Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 function Pin({ item }) {
   return (
